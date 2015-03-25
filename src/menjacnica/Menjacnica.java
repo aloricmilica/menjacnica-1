8f3102883f3728cdf14669b431e89c251c210790
aloricmilica <aloricmilica@yahoo.com>
2015-03-25 19:28:05 +0100

    Napravljena nova klasa Menjacnica koja implementira sve metode iz interfejsa MenjacnicaInterfejs Metode su sa "praznim" telom

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void izbrisiKurs(Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public LinkedList<Kurs> pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
