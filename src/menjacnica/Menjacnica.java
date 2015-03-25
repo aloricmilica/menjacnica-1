a4fcda36e9b3a56556b457745d494952c0c57db6
aloricmilica <aloricmilica@yahoo.com>
2015-03-25 20:43:45 +0100

    Do kraja implementirana klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -6,20 +6,34 @@ import java.util.LinkedList;
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursnaLista = new LinkedList<Kurs>();
 
 	public void dodajKurs(Kurs kurs) {
-		// TODO Auto-generated method stub
+		if(kurs == null || kursnaLista.contains(kurs))
+			throw new RuntimeException("Kurs vec postoji u kursnoj listi ili ga niste pravilno uneli");
+		kursnaLista.add(kurs);
 
 	}
 
 	public void izbrisiKurs(Kurs kurs) {
-		// TODO Auto-generated method stub
+		if(!kursnaLista.contains(kurs)) 
+			throw new RuntimeException("Kurs nije u kursnoj listi");
+		kursnaLista.remove(kurs);
 
 	}
 
 	public LinkedList<Kurs> pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		LinkedList<Kurs> rezultat = new LinkedList<Kurs>();
+		
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(valuta == null  || datum == null || !kursnaLista.get(i).getDatum().equals(datum) 
+					|| !kursnaLista.get(i).getDatum().equals(datum))
+				throw new RuntimeException("Niste uneli valutu i datum, ili ne postoji kurs za trazeni datum");
+					rezultat.add(kursnaLista.get(i));
+			}
+		return rezultat;
 	}
 
 }
